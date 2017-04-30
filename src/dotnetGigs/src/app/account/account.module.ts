3f5cf187bdf9a8a326a80f39b60cc00a8c5a6a28
Mark Macneil <mark.macneil@t4g.com>
2017-04-30 14:12:34 -0300

    post in progress

--- src/dotnetGigs/src/app/account/account.module.ts
+++ src/dotnetGigs/src/app/account/account.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [RegistrationFormComponent]
 })
 export class AccountModule { }
---
+++ src/dotnetGigs/src/app/account/registration-form/registration-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.scss']
+})
+export class RegistrationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
